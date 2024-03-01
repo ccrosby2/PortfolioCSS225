5be4d4af0b394881fd8eca470688909aac141c01
ccrosby2 <157847720+ccrosby2@users.noreply.github.com>
2024-03-01 16:46:21 -0600

    Update chapter5m8.py

--- chapter5m8.py
+++ chapter5m8.py
@@ -1,5 +1,6 @@
 from Gvars import*
 import random
+import turtle
 def pick_name():
 
 name = input("Choose who you want. 1. Cleo, 2. Stoney, 3. TT, 4. Frankie")
@@ -11,20 +12,18 @@ name = input("Choose who you want. 1. Cleo, 2. Stoney, 3. TT, 4. Frankie")
     
     if name == ("2"):
         name = "TT"
-        print("Let's go to the agency and get my son so we can leave out the country.")
     elif name == "3":
         name = ("Stoney")
-        print("Let go to the hood and ask one of them men to give us some money.")
     elif name == "4":
         name =("Frankie")
-        print ("Let's go to the bank and get some more of the money from the cops.")
     elif name == "1":
         name = "Cleo"
-        print("Let's go rob Bank Federal Downtown.")
     else:
         print("That is not a option. Please choose again.")
     for num in names:
         print (num) 
+    if name = "1"
+    Cleo = turtle.turle
                     #names.remove("Stoney")
                     #names.remove("Frankie")
                     #names.remove("Cleo")
