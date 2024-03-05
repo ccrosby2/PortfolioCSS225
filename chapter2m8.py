f0ea1a33197aa6d73727aacb44d42e7f363a03e2
ccrosby2 <157847720+ccrosby2@users.noreply.github.com>
2024-03-05 12:06:12 -0600

    Update chapter2m8.py

--- chapter2m8.py
+++ chapter2m8.py
@@ -30,80 +30,87 @@ if option == "0":
 elif option== "1":
     print ("Stoney says")
     print ("I want to get the outfits")
-
 elif option == "4":
 	print("This is not a option. Game over")
-
 elif option == "3":
     print("TT says")
     print("I want to get my son from the agency before we leave.")
+elif option =="5":
+    if name == "Cleo":
+        print ("Stoney,TT,and Frankie can get us a new ride")
+    else:
+        print("You have made the wrong choice.")
 for num in names:
     print (num)
 
-option_inventory=input("What option you want to take"):
+option_inventory=input("What option you want to take")
 Cleo = "0"
 Stoney = "1"
 Frankie = "2"
 TT = "3"
 
 if option_inventory == "2":
-print ("I want to get some jumpsuits, mask, and guns to rob the bank downtown")
+    print ("I want to get some jumpsuits, mask, and guns to rob the bank downtown")
 
 elif option_inventory == "1":
     print ("I want to get the outfits")
 
-elif option_inventory == 0:
+elif option_inventory == "0":
 	print("This is not a option. Game over")
 
 elif option_inventory == "3":
-print("I want to get my son from the agency before we leave.")
+    print("I want to get my son from the agency before we leave.")
 
-elif option == (x,id)
+elif type(option)== int:
     print("This is not a option. Try again")
 
-class person
-def __init__ (self, inventory,jumpsuits, outfits, masks, agency,mall,anti_keytype):
-		self.inventory = inventory
-		self.jumpsuits = jumpsuits
-		self.masks= masks
-		self.outfits = outfits
-		self.agency =  agency
-   		self.mall = mall
-		self.anti_keytype= anti_keytype
-
-def get_inventory(self): 
-return 
-  self.Cleo = print ("I want to get some jumpsuits, mask, and guns to rob the bank downtown")
-  self.Frankie= print ("I want to get the outfits")
-  self.TT= print("I want to get my son from the agency before we leave."
-  self.Stoney= ("I want to get the shoes from the mall.")
-def get_masks(self):
- return print ("We do need this but I need a new car,Im not going to that Agency")
-def get_jumpsuits(self):
-  return print ("I already got the jumpsuits. We need money.")
-def get_outfits(self):
- return print("We need more.")
-def get_agnecy(self)
-  return print("TT! They not giving you your child unless you go rob Bank Federal Downtown.")
-def get_anti_keytype():
-    while running:
-	pyautogui.press(key_2[randint(0,2)])
-	return print("This is not a option.")
-def fullperson(self):
-		return self.inventory + ' ' + str(self.jumpsuit) + ' ' + self.masks + ' ' + self.agency + ' ' + self.mall
-
-person1 = person(Cleo_option: "Smith and Wesson 669", 2012,"blue jumpsuits", "seethru masks", "We not going to the Agency to get your son","We can not go to a mall.We going to be seen.")
-person2 = person((TT_option: "I dont want to kill nobody", pink jumpsuits, "diamond masks", "I want to get my son", "I like malls")
-person3 = person((Stony_option: "I got a Smith and Wesson 4505, black jumpsuits, "wigs that cover us like masks", "Your son gone be ok unitl after we rob this bank","the mall better than blue/pink jumpsuits."
-person4 = person((Frankie_option: "I got a friend but no weapon", "no jumpsuit","I want to go get your son though TT","the mall too open for us right now,")
+class person:
+    def __init__ (self, inventory,jumpsuit, outfits, masks, agency,mall):
+        self.inventory = inventory
+        self.jumpsuit = jumpsuit
+        self.masks= masks
+        self.outfits = outfits
+        self.agency =  agency
+        self.mall = mall
+    
+    def get_inventory(self): 
+        self.Cleo = print ("I want to get some jumpsuits, mask, and guns to rob the bank downtown")
+        self.Frankie= print ("I want to get the outfits")
+        self.TT= print("I want to get my son from the agency before we leave.")
+        self.Stoney= ("I want to get the shoes from the mall.")
+    def get_masks(self):
+        print ("We do need this but I need a new car,Im not going to that Agency")
+        return
+    def get_jumpsuit(self):
+        print ("I already got the jumpsuits. We need money.")
+        return
+    def get_outfits(self):
+        print("We need more.")
+        return
+    def get_agency(self):
+        print("TT! They not giving you your child unless you go rob Bank Federal Downtown.")
+        return
+            
+    def get_anti_keytype():
+        while running:
+            pyautogui.press(key_2[randint(0,2)])
+            print("This is not a option.")
+            return
+    def fullperson(self):
+        return self.inventory + ' ' + str(self.jumpsuit) + ' ' + self.masks + ' ' + self.agency + ' ' + self.mall
+    
+person1 = person("Smith and Wesson 669","blue jumpsuits", "we need a outfit","seethru masks", "We not going to the Agency to get your son","We can not go to a mall.We going to be seen.")
+person2 = person("I dont want to kill nobody", "pink jumpsuits","we need a outfit", "skull masks", "I want to get my son", "I like malls")
+person3 = person("I got a Smith and Wesson 4505", "black jumpsuits","we need a outfit", "wigs like masks", "Your son gone be ok unitl after we rob this bank","the mall better than blue/pink jumpsuits.")
+person4 = person("I got a friend but no weapon", "no jumpsuit","we need a outfit","diamond masks","I want to go get your son though TT","the mall too open for us right now,")
 
 print(person1.get_inventory())
-print(person1.get_jumpsuits())
+print(person1.get_jumpsuit())
 print(person1.get_masks())
-print(person2.get_jumpsuits()) 
+print(person2.get_jumpsuit()) 
 print(person2.get_masks())
-prnt(person3.get_inventory())
-print(person3.get_jumpsuits())
+print(person3.get_inventory())
+print(person3.get_jumpsuit())
 print(person3.get_masks())
 
 print(person4)
@@ -112,7 +119,7 @@ print(person2.fullperson())
 print(person3.fullperson())
 print(person4.fullperson())
 
-num= "cleo", "stoney, "tt", "frankie"
+num= ["cleo", "stoney", "tt", "frankie"]
 jumpsuits= "0"
 masks = "1"
 outfits = "2"
@@ -121,8 +128,7 @@ mall = "4"
 inventory="5"
 num2 = "6"
 
-if option== input("1. jumpsuits, 2. masks, 3. outfits, 4. agency, 0.mall,5. inventory,6.num, 7. get_inventory)
-
+option= input("1. jumpsuits, 2. masks, 3. outfits, 4. agency, 0.mall,5. inventory,6.num, 7. get_inventory")
 jumpsuits= "0"
 masks = "1"
 outfits = "2"
@@ -132,10 +138,10 @@ inventory="5"
 num = "6"
 get_inventory="7"
 
- elif option == num
+if option == num:
  print ("Cleo is getting us ready for our next heist")
- elif option!= num2
+elif option!= num2:
  print ("Game over")
-elif option= [7']
+elif option== "7":
  args [input(get_inventory)]
  print("This is what the next heist clothes.")
