cce354c00dbe3586ce68c102a7cf97c27ffdced8
ccrosby2 <157847720+ccrosby2@users.noreply.github.com>
2024-03-05 12:17:25 -0600

    Update inventory.py

--- inventory.py
+++ inventory.py
@@ -1,6 +1,6 @@
 inventory= "get_inventory"
-jumpsuits=print ("we are wearing this for the next heist.")
-inventory_Cleo= print("seethru_masks","blue_clothes","job_prepared","identity_covered")
-inventory_TT= print ("wigs_nomask","regular_clothes","mall_prepared","identity_see")
-inventory_Frankie= print("diamond_masks","regular_clothes","no_mall","no_agency")
-inventory_Stoney= print("skull_masks","mall_prepared","mall","agency")
+jumpsuits=("we are wearing this for the next heist.")
+inventory_Cleo=("seethru_masks","blue_clothes","job_prepared","identity_covered")
+inventory_TT=("wigs_nomask","regular_clothes","mall_prepared","identity_see")
+inventory_Frankie=("diamond_masks","regular_clothes","no_mall","no_agency")
+inventory_Stoney=("skull_masks","mall_prepared","mall","agency")
