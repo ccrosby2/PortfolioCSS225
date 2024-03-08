77c1f4b0a943e5cd1de4b9ba87916a3e74b912d7
ccrosby2 <157847720+ccrosby2@users.noreply.github.com>
2024-03-08 14:41:19 -0600

    Update chapter5m8.py

--- chapter5m8.py
+++ chapter5m8.py
@@ -1,25 +1,25 @@
 #from Gvars import*
 import random
-import tkinter as TK
-def pick_name():
+import turtle
 
-    name = input("Choose who you want. 1. Cleo, 2. Stoney, 3. TT, 4. Frankie")
-    num.turtle() == "Frankie", "Stoney", "TT","Cleo"
-    
-    while name not in ["1","2","3","4"]:
-        print("This is not a option. Game Over")
-        name = input("Choose who you want. 1. Cleo, 2. TT, 3. Stoney, 4. Frankie")
-    
-    if name == ("2"):
-        name = "TT"
-    elif name == "3":
-        name = ("Stoney")
-    elif name == "4":
-        name =("Frankie")
-    elif name == "1":
-        name = "Cleo"
-    else:
-        print("That is not a option. Please choose again.")
+num = int(input("Choose who you want. 1. Cleo, 2. Stoney, 3. TT, 4. Frankie"))
+name = ["Frankie", "Stoney", "TT","Cleo"]
+
+while name not in ["1","2","3","4"]:
+    print("This is not a option. Game Over")
+    name = input("Choose who you want. 1. Cleo, 2. TT, 3. Stoney, 4. Frankie")
+
+if name == ("2"):
+    name = "TT"
+elif name == "3":
+    name = ("Stoney")
+elif name == "4":
+    name =("Frankie")
+elif name == "1":
+    name = "Cleo"
+else:
+    print("That is not a option. Please choose again.")
+
 while num.turtle: ("Cleo")
 Cleo = turtle.turle
 def draw_(t, sz):
@@ -67,5 +67,3 @@ main()
                     #names.remove("Cleo")
                     #names.remove("TT")
                     #print("Game Over")
-
-
