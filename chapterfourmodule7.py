d5857c59a293de1f9b2a36513a96e1067cb75a15
ccrosby2 <157847720+ccrosby2@users.noreply.github.com>
2024-03-08 13:20:18 -0600

    Update chapterfourmodule7.py

--- chapterfourmodule7.py
+++ chapterfourmodule7.py
@@ -1,31 +1,34 @@
 import random
 
-tbl1=bun
-tbl2=fries
-tbl3=drink
-tbl4=hotdog
-tbl5=salad w/no meat
-tbl6=large drink
-tbl7=crab
-tbl8=spaghetti
-tbl9=salad
-tbl10=soupw/chicken
-tbl11=small drink
-tbl12=luguine pasta w/desert 
+option = (1,12)
+int = random.randrange(1,12)
+burger="burger"
+fries="fries"
+drink = "drink"
+hotdog = "hotdog"
+salad= "salad" 
+large_drink = "large_drink"
+crab = "crab"
+spaghetti = "spaghetti"
+soup_chicken = "soupw/chicken"
+small_drink = "small drink"
+Luguine="luguine pasta desert" 
+hungry = ("burger")or("hotdog")or("crab")or("spaghetti")or("soup_chicken")
+option= input("Choose dinner:")
+print(input)
 
-hungry = {'tbl1': option ,"tbl2" : option_one, tbl3:option_three} 
-inp = int(input("Choose dinner:")) 
- 	print(input) 
-
+num_attempts = 9
+if num_attempts > 9:
+    print ("Please order something to eat.")
 print("Banker responses")
-while option(hungry):
-    print(option)
+if option == hungry:
     print("Bank manager says:   I am a vegan")
-if badfood(option_one):
-    print("This restaurant is pretty good."
-else:
+elif option == Luguine:
+    print("This restaurant is pretty good.")
+    print("You can move to chapter five")
+elif option == hungry:
     print("This is not a option")
-    print("option_three")
-
+else:
+    print("Game Over.")    
 
 
