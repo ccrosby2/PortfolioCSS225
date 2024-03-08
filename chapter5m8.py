94e7dbccb9304811eb327bfbdac34a1282c0a22b
ccrosby2 <157847720+ccrosby2@users.noreply.github.com>
2024-03-08 17:00:09 -0600

    Update chapter5m8.py

--- chapter5m8.py
+++ chapter5m8.py
@@ -1,3 +1,124 @@
+#from Gvars import*
+import random
+import turtle
+
+num = input("Choose who you want. 1. Cleo, 2. Stoney, 3. TT, 4. Frankie")
+#name = ["Frankie", "Stoney", "TT","Cleo"]
+
+while num not in ["1","2","3","4"]:
+    print("This is not a option. Game Over")
+    name = input("Choose who you want. 1. Cleo, 2. TT, 3. Stoney, 4. Frankie")
+
+if num == ("2"):
+    name = "TT"
+elif num == "3":
+    name = ("Stoney")
+elif num == "4":
+    name =("Frankie")
+elif num == "1":
+    name = "Cleo"
+else:
+    print("That is not a option. Please choose again.")
+print (name)
+alex = turtle.Turtle()
+wn = turtle.Screen()
+def draw_(t, sz):
+    for i in range(4):
+        t.forward(sz)
+        t.left(90)
+alex.color("black")
+	
+#wn.exitonclick()
+jim=turtle.Turtle()
+jim.setpos(0, -20)
+   
+jim.left(90)
+    
+    # draw body
+jim.color("black")
+jim.forward(60)
+jim.pendown()
+    
+    # draw head
+jim.color("black")
+jim.forward(90)
+jim.circle(45)
+    
+    # move to position
+jim.penup()
+jim.setpos(20, -20) #Sets the position of the turtle
+jim.left(90)
+jim.pendown()
+    
+#main()
+                    #names.remove("Stoney")
+                    #names.remove("Frankie")
+                    #names.remove("Cleo")
+                    #names.remove("TT")
+                    #print("Game Over")
+                    
+
+
+#from Gvars import*
+import random
+import turtle
+
+num = input("Choose who you want. 1. Cleo, 2. Stoney, 3. TT, 4. Frankie")
+#name = ["Frankie", "Stoney", "TT","Cleo"]
+
+while num not in ["1","2","3","4"]:
+    print("This is not a option. Game Over")
+    name = input("Choose who you want. 1. Cleo, 2. TT, 3. Stoney, 4. Frankie")
+
+if num == ("2"):
+    name = "TT"
+elif num == "3":
+    name = ("Stoney")
+elif num == "4":
+    name =("Frankie")
+elif num == "1":
+    name = "Cleo"
+else:
+    print("That is not a option. Please choose again.")
+print (name)
+alex = turtle.Turtle()
+wn = turtle.Screen()
+def draw_(t, sz):
+    for i in range(4):
+        t.forward(sz)
+        t.left(90)
+alex.color("black")
+	
+#wn.exitonclick()
+jim=turtle.Turtle()
+jim.setpos(0, -20)
+   
+jim.left(90)
+    
+    # draw body
+jim.color("black")
+jim.forward(60)
+jim.pendown()
+    
+    # draw head
+jim.color("black")
+jim.forward(90)
+jim.circle(45)
+    
+    # move to position
+jim.penup()
+jim.setpos(20, -20) #Sets the position of the turtle
+jim.left(90)
+jim.pendown()
+    
+#main()
+                    #names.remove("Stoney")
+                    #names.remove("Frankie")
+                    #names.remove("Cleo")
+                    #names.remove("TT")
+                    #print("Game Over")
+                    
+
 
 #from Gvars import*
 import random
