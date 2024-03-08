4a8317b3a2b8168399dac33da71836c549751807
ccrosby2 <157847720+ccrosby2@users.noreply.github.com>
2024-03-08 12:50:18 -0600

    Update chapter3m8.py

--- chapter3m8.py
+++ chapter3m8.py
@@ -1,112 +1,45 @@
-from Gvars import *
-def pick_name():
-import chapter2m8
-chapter2m8.pick_name()
 import random
 
 option = (1,5)
 user = random.randrange(1,5)
-user_guess = "6"
 Cleopatria_Lenchrich = "0"
 Patrick_Owens ="2"
 Cameron_lens ="3"
 Max_House ="4"
 Lida_Williams="5"
-if option == 6:
-    print("This is not a option. Game over")
-elif option == [1,2,3,4,5]
+user = int(input("Guess a number between 1 and 5"))
+if user == 6:
+    print("This is not a option. Game over")   
+elif 0 < user and user < 6:
     print ("Lets conduct a investigation")
+else:
+    print ("This is not a option. Please leave the investigation room.")
+user = -1
+Cleopatria_Lenchrich = 0
+Patrick_Owens =2
+Cameron_lens =3
+Max_House =4
+Lida_Williams=5
+num_guesses =0
 
-user_guess = "1"
-Cleopatria_Lenchrich = "0"
-Patrick_Owens ="2"
-Cameron_lens ="3"
-Max_House ="4"
-Lida_Williams="5"
-
-while user != user_guess:
+while user != 0 and num_guesses < 3:
   user = int(input("Enter a number 0-5"))
+  num_guesses +=1
   if user == 1:
-    print("This is not a option. Game Over.")
-  elif user_guess(1) >= user:
+    print("You guessed a wrong number. Game Over.")
+  elif user == 2:
     print("This is not the person who killed Luther!")
     print ("Please guess again.")
-  elif user >=user_guess(3):
+  elif user == 3:
     print("This not a girl")
     print("Please guess again.")
-  elif user<= userguess(2,4,5)
+  elif user == 4 or user == 5:
     print("These might be the girls that killed Luther.")
-elif user == userguess(0)
+  elif user == 0:
     print("This is the person that Killed Luther.Do you know if this the person?")
-
-
-
-
-
-
-
-import random
-(n) = random.randrange(1,5)
-user = 0-5
-n =input(0+1)
-
-Cleopatria_Lenchrich="1"
-Patrick_Owens= "2"
-Cameron_lens="3"
-Max_House= "4"
-Lida_Williams="5"
-lose= "0"
-"This is not a option. Game over"
-while user != user:
-  user = int(input("Enter a number 0-5"))
-  if user == 0:
-    print("This is not a option. Game Over.")
-    
-  elif user >= ["n,(num,3)"]:
-    print("This not a girl")
-    print("Please guess again.")
-
-
-if option == Cleopatria_Lenchrich:
-    print("This is the person who killed Luther!")
-elif option == Patrick_Owens:
-    print("This is a boy")
-elif option == Cameron_lens:
-    print("This is a girl but not the person who killed Luther.")
-elif option == Max_House:
-    print("This is a person but this is a boy")
-elif option == Lida_Williams:
-    print("This is not who killed Luther.")
-option = "0"
-Cleopatria_Lenchrich="1"
-Patrick_Owens= "2"
-Cameron_lens="3"
-Max_House= "4"
-Lida_Williams="5"
-
-import random
-user = random.randrange(1,5)
-option = "0"
-user = int(input("Enter a number 0-5"))
-Cleopatria_Lenchrich="Cleopatria Lenchrich"
-Patrick_Owens= "Patrick Owens"
-Cameron_lens="Cameron lens"
-Max_House= "Max House"
-Lida_Williams="Lida Williams"
-print("Choose a name to arrest in this list")
-print(Cleopatria_Lenchrich)
-print (Patrick_Owens)
-print(Cameron_lens)
-print(Max_House)
-print(Lida_Williams)
-Cameron_lens="3"
-Max_House= "4"
-Lida_Williams="5"
-
-if option == 0:
-    print("This the person who killed Luther")
-elif option < 5:
-    print("This is not the person who killed Luther")
-
-
-
+  else:
+    print("This is not a option. Please guess again.")
+if num_guesses==3:
+    print("You ran out of tries. Game over")
+else:
+    print("Lets go to chapter 3.")
